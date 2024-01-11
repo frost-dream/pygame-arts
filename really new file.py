749bab5a2568b9dc8f58227c6bad37f0ee3df562
frost-dream <127839308+frost-dream@users.noreply.github.com>
2024-01-11 21:48:37 +0330

    really new file.py

--- really new file.py
+++ really new file.py
@@ -37,26 +37,22 @@ while True:
                 y1 = line['y'] + line['timer'] // 5 * sin(line['angle'])
                 x2 = other_line['x'] + other_line['timer'] // 5 * cos(other_line['angle'])
                 y2 = other_line['y'] + other_line['timer'] // 5 * sin(other_line['angle'])
-                distance = dist((line['x'], line['y']), (other_line['x'], other_line['y']))
-                if distance < 50:
+                if dist((line['x'], line['y']), (other_line['x'], other_line['y'])) < 50:
                     alpha = min([120, line['timer']])
                     l(screen, (alpha, alpha, alpha), (line['x'], line['y']), (other_line['x'], other_line['y']), 1)
                     circle(screen, (alpha, alpha, alpha), (line['x'], line['y']), 2)
                     circle(screen, (alpha, alpha, alpha), (other_line['x'], other_line['y']), 2)
-                distance = dist((line['x'], line['y']), (x2, y2))
-                if distance < 50:
+                if dist((line['x'], line['y']), (x2, y2)) < 50:
                     alpha = min([120, line['timer']])
                     l(screen, (alpha, alpha, alpha), (line['x'], line['y']), (x2, y2), 1)
                     circle(screen, (alpha, alpha, alpha), (line['x'], line['y']), 2)
                     circle(screen, (alpha, alpha, alpha), (x2, y2), 2)
-                distance = dist((x1, y1), (x2, y2))
-                if distance < 50:
+                if dist((x1, y1), (x2, y2)) < 50:
                     alpha = min([120, line['timer']])
                     l(screen, (alpha, alpha, alpha), (x1, y1), (x2, y2), 1)
                     circle(screen, (alpha, alpha, alpha), (x1, y1), 2)
                     circle(screen, (alpha, alpha, alpha), (x2, y2), 2)
-                distance = dist((x1, y1), (other_line['x'], other_line['y']))
-                if distance < 50:
+                if dist((x1, y1), (other_line['x'], other_line['y'])) < 50:
                     alpha = min([120, line['timer']])
                     l(screen, (alpha, alpha, alpha), (x1, y1), (other_line['x'], other_line['y']), 1)
                     circle(screen, (alpha, alpha, alpha), (x1, y1), 2)
@@ -84,4 +80,4 @@ while True:
             line['x'] += cos(angle_to_mouse) * 5
             line['y'] += sin(angle_to_mouse) * 5
     flip()
-    clock.tick(100)
+    clock.tick(60)
